7a40de8806a12021f83035874713a1a9b85545a8
tawing101 <98728065+tawing101@users.noreply.github.com>
2023-08-11 12:37:04 +0800

    Update raids.ts

--- src/roo/schedule/raids.ts
+++ src/roo/schedule/raids.ts
@@ -1,23 +1,23 @@
 import { ScheduleTime } from '.';
 
 export enum Raid {
-	MrPartyAndGuildRaid,
-	MrPartyAndGuildRaid,
-	MrPartyAndGuildRaid,
+	1MrPartyAndGuildRaid,
+	2MrPartyAndGuildRaid,
+	3MrPartyAndGuildRaid,
 }
 
-export const raids = [Raid.MrPartyAndGuildRaid, Raid.MrPartyAndGuildRaid, Raid.MrPartyAndGuildRaid];
+export const raids = [Raid.1MrPartyAndGuildRaid, Raid.2MrPartyAndGuildRaid, Raid.3MrPartyAndGuildRaid];
 
 export const getRaidTime = (value: Raid): ScheduleTime => {
 	switch (value) {
-		case Raid.MrPartyAndGuildRaid:
+		case Raid.1MrPartyAndGuildRaid:
 			return { hours: 9, minutes: 0 };
 
-		case Raid.MrPartyAndGuildRaid:
+		case Raid.2MrPartyAndGuildRaid:
 			return { hours: 12, minutes: 33 };
 
 		// case Raid.AuctionStart:
-		case Raid.MrPartyAndGuildRaid:
+		case Raid.3MrPartyAndGuildRaid:
 			return { hours: 20, minutes: 30 };
 	}
 };
