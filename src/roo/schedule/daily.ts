3b42c1dc1748b0febd731d4dad84ebce807d93b5
tawing101 <98728065+tawing101@users.noreply.github.com>
2023-08-11 14:19:32 +0800

    Update daily.ts

--- src/roo/schedule/daily.ts
+++ src/roo/schedule/daily.ts
@@ -95,7 +95,7 @@ export const getDailyTime = (value: Daily): ScheduleTime => {
 			return { hours: 10, minutes: 0 };
 
 		case Daily.MRPartyAndGHRaid:
-			return { hours: 13, minutes: 17 };
+			return { hours: 13, minutes: 22 };
 			
 		case Daily.GuildFeast:
 		case Daily.GuildQuiz:
